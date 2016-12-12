11b3deafaed979036ed62c1afdca69ecdbae61f7
thomas danner <thmsdnnr@gmail.com>
2016-12-12 01:30:57 +0000

    first working commit sweeeeeeet

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express=require('express');
+var multer=require('multer');
+var upload=multer({dest:'uploads/'});
 var app=express();
-app.get('/',function(req,res){
-res.send('hello world');
+var path=require('path');
+app.use(express.static('/'));
+app.post('/submit',upload.single('upfile'),function(req,res,next){
+res.send({"size":req.file.size});
+res.end();
 });
+app.get('*',function(req,res){res.sendFile(path.join(__dirname, 'index.html'));});
+
 app.listen(process.env.PORT||8080, function() {console.log('listening on port '+process.env.PORT||8080);});
